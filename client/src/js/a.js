a22539322b7f182db0c8df8078c16d5109ea316a
ammar.haider <ammar.haider@oakbrookfinance.com>
2016-12-02 17:53:04 +0000

    playing with require.ensure

---
+++ client/src/js/a.js
@@ -0,0 +1,3 @@
+export default () => {
+	console.log('require.ensure working');
+};
--- client/src/js/main.jsx
+++ client/src/js/main.jsx
@@ -20,17 +20,26 @@ setTimeout(printAfterTimeout, 1500);
 // setTimeout(printAfterTimeout2, 1500);
 
 const CommentBox = React.createClass({
+
+  reqEnsureTest() {
+    console.log('click on img');
+    require.ensure([], require => {
+      const a = require('./a');
+    });
+  },
+
   render() {
     return (
       <div className="commentBox">
         <p>Hello world! I am a client-side react component.</p>
         <CjsxComponent />
-        <p>This is an image</p>
+        <p onClick={ this.reqEnsureTest }>This is an image</p>
         <img alt="radiohead" src={imgSrc} style={{ height: '20%', width: '20%' }} />
         <p>This is a star icon <span className="glyphicon glyphicon-star"></span></p>
         <span className="label" style={{color: 'grey'}}>This is a label</span>
       </div>
     );
+
   }
 });
 
