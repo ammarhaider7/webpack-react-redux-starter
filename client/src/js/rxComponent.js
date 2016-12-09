5380a156cad0356517c78ed044359facdb008efc
Ammar Haider <ammar.haider@oakbrook.local>
2016-12-09 14:24:50 +0000

    concurrent http requests using forkJoin in rxjs

--- client/src/js/rxComponent.js
+++ client/src/js/rxComponent.js
@@ -2,23 +2,28 @@ import React, { Component } from 'react';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/dom/ajax'
 import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/forkJoin';
 
 export default class RxComponent extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			users: []
+			users: [],
+			todos: []
 		}
 	}
 
 	componentDidMount() {
+
+		const users = Observable.ajax('https://jsonplaceholder.typicode.com/users').map(res => { return res.response });
+		const todos = Observable.ajax('https://jsonplaceholder.typicode.com/todos').map(res => { return res.response });
 		
-		Observable.ajax('https://jsonplaceholder.typicode.com/users')	
-			.map(res => { return res.response })
-			.subscribe(users => {
+		Observable.forkJoin([users, todos])	
+			.subscribe(results => {
 				this.setState({
-					users
+					users: results[0],
+					todos: results[1]
 				})
 			})
 
@@ -27,8 +32,13 @@ export default class RxComponent extends Component {
 	render() {
 		return (
 			<div>
+				<h3>First group</h3>
 				<ul className="rxComponent">
 					{this.state.users.map(user => <li key={user.id}>{user.name}</li>)}
+				</ul>
+				<h3>Second group</h3>
+				<ul className="rxComponent">
+					{this.state.todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
 				</ul>
 			</div>
 		);
